721f2d95a24d9774da3dbaee6e2f28bdc10c6fab
codeaowl <dejanpetrovicp@outlook.com>
2023-10-30 17:21:29 +0200

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("product", r -> r.path("/api/**")
+			.filters(f -> f.stripPrefix(1))
+			.uri("lb://PRODUCER-SERVICE"))
+			.build();
+	}
+}
